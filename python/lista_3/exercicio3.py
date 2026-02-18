24aeabd1449412602d6c74b0ff9a92cff9530e3d
Elisabete-MO <bete.mrt@gmail.com>
2026-02-18 20:53:10 -0300

    add exercise 3 file

--- python/lista_3/exercicio3.py
+++ python/lista_3/exercicio3.py
@@ -1,9 +1,14 @@
 # Crie uma função chamada criar_email que recebe nome, sobrenome e dominio (com valor padrão "empresa.com.br") e retorna um email no formato "nome.sobrenome@dominio".
 
+nome = input("Digite o nome: ")
+sobrenome = input("Digite o sobrenome: ")
+dominio = input("Digite o dominio: ")
+
 def criar_email(nome, sobrenome, dominio="empresa.com.br"):
   email = f"{nome.lower()}.{sobrenome.lower()}@{dominio.lower()}"
   return email
 
+print(criar_email(nome, sobrenome, dominio))
 
 # Testes rápidos
 print(criar_email("Ana", "Silva"))
